4cfe5e0622c75a29f23094c1bd02fb852ba82d4e
Parkcw00 <parkcw0325@gmail.com>
2024-11-28 15:07:14 +0900

    ✨ update  아이템라우터 추가

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import AccountsRouter from "./routes/accounts.router.js";
 import LogMiddleware from "./middlewares/log.middleware.js";
 import ErrorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
 import CharactersRouter from "./routes/characters.router.js";
+import ItemsRouter from "./routes/items.router.js";
 
 const app = express();
 const PORT = 3001;
@@ -14,7 +15,7 @@ app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", [AccountsRouter, CharactersRouter]);
+app.use("/api", [AccountsRouter, CharactersRouter, ItemsRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
