90d32fac8307d5b7c6838789eead43ef0e355a30
Parkcw00 <parkcw0325@gmail.com>
2024-11-28 14:03:35 +0900

    ✨ update  로그미들,에러미들,캐릭터 라우터 추가

--- src/app.js
+++ src/app.js
@@ -3,13 +3,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import AccountsRouter from "./routes/accounts.router.js";
+import LogMiddleware from "./middlewares/log.middleware.js";
+import ErrorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
+import CharactersRouter from "./routes/characters.router.js";
 
 const app = express();
 const PORT = 3001;
 
+app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api", [AccountsRouter]);
+
+app.use("/api", [AccountsRouter, CharactersRouter]);
+app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸어요!");
